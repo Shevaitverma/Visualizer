e483418d6cde1c85d16ca87be9c79417bc894b01
Shevaitverma <shevaitverma333@gmail.com>
2023-12-27 16:29:17 +0530

    basic configuration for frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Test from "./components/test";
+
+function App() {
+  return (
+    <>
+    <div>
+      <h2>This is root compartment</h2>
+    </div>
+    <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+        <h3>This is a component</h3>
+    </div>
+  )
+}
+
+export default Test
